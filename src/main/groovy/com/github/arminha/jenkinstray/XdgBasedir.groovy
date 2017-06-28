80dde5594d7ef9e9b57dff1a02a3316fdff19d97
Armin Häberling <armin.aha@gmail.com>
2017-06-28 20:24:36 +0200

    Choose sensible config file path on Windows

--- src/main/groovy/com/github/arminha/jenkinstray/XdgBasedir.groovy
+++ src/main/groovy/com/github/arminha/jenkinstray/XdgBasedir.groovy
@@ -5,17 +5,30 @@ import groovy.transform.CompileStatic
 import java.nio.file.Path
 import java.nio.file.Paths
 
+/**
+ * Implements parts of the XDG directory specification with sensible fallbacks on Windows systems.
+ */
 @CompileStatic
 class XdgBasedir {
   static final String XDG_CONFIG_HOME = "XDG_CONFIG_HOME"
 
+  /**
+   * Returns the XDG_CONFIG_HOME directory.
+   */
   static Path configHome() {
     def env = System.getenv()
+    if (isWindows()) {
+      return Paths.get(env['LOCALAPPDATA'])
+    }
     def value = env[XDG_CONFIG_HOME]?.trim()
     if (value) {
       Paths.get(value)
     } else {
       Paths.get(env['HOME'], '.config')
     }
+  }
+
+  private static boolean isWindows() {
+    System.getProperty('os.name').toLowerCase().startsWith('windows')
   }
 }
