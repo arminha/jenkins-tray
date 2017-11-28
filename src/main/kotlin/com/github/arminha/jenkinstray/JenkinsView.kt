363f7fb76f59c76ad7485675c1f66e6f3a2c66f6
Armin Häberling <armin.aha@gmail.com>
2017-11-28 20:26:19 +0100

    Replace httpclient with okhttp

--- src/main/kotlin/com/github/arminha/jenkinstray/JenkinsView.kt
+++ src/main/kotlin/com/github/arminha/jenkinstray/JenkinsView.kt
@@ -5,36 +5,36 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.arminha.jenkinstray.data.JenkinsStatus
 import com.github.arminha.jenkinstray.data.Job
-import org.apache.http.HttpHeaders
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
 import java.nio.charset.StandardCharsets
 import java.util.Base64
 import kotlin.collections.ArrayList
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class JenkinsView(val url: String, val username: String?, val accessToken: String?) {
     companion object {
-        val httpClient = HttpClients.createDefault()
+        val httpClient = OkHttpClient()
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     data class JobList(val jobs: List<Job>)
 
     fun retrieveJobs(): List<Job> {
-        val request = HttpGet(url + "api/json?tree=jobs[name,color,lastBuild[number,result,timestamp]]")
+        val request = Request.Builder()
+                .url(url + "api/json?tree=jobs[name,color,lastBuild[number,result,timestamp]]")
         if (username != null && accessToken != null) {
-            val auth = username + ":" + accessToken;
+            val auth = username + ":" + accessToken
             val encodedAuth = Base64.getMimeEncoder().encodeToString(
-                    auth.toByteArray(StandardCharsets.ISO_8859_1));
-            val authHeader = "Basic " + encodedAuth;
-            request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
+                    auth.toByteArray(StandardCharsets.ISO_8859_1))
+            val authHeader = "Basic " + encodedAuth
+            request.addHeader("Authorization", authHeader)
         }
         val mapper = jacksonObjectMapper()
-        val response = httpClient.execute(request)
+        val response = httpClient.newCall(request.build()).execute()
         val list = response.use {
-            val status = response.statusLine.statusCode
+            val status = response.code()
             if (status in 200..299) {
-                val content = response.entity.content.readBytes()
+                val content = response.body()!!.bytes()
                 try {
                     mapper.readValue<JobList>(content)
                 } catch (e: Exception) {
