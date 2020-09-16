d0fb147ef96f3847d502bd494deb5b85fab62735
Armin Häberling <armin.aha@gmail.com>
2020-09-16 18:18:59 +0200

    Use Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+  groovy
+  application
+  id("org.jetbrains.kotlin.jvm").version("1.3.70")
+  id("edu.sc.seis.launch4j").version("2.4.6")
+}
+
+val appMainClass = "com.github.arminha.jenkinstray.Main"
+val jvmOpts = listOf("-Xms10m", "-Xmx32m", "-Xss228k", "-XX:+UseSerialGC")
+
+application {
+  mainClass.set(appMainClass)
+  applicationDefaultJvmArgs = jvmOpts
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("com.beust:klaxon:5.2")
+  implementation("com.squareup.okhttp3:okhttp:3.14.7")
+  implementation("com.moandjiezana.toml:toml4j:0.7.2")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  testImplementation("org.codehaus.groovy:groovy:2.5.10")
+  testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
+}
+
+launch4j {
+  mainClassName = appMainClass
+  dontWrapJar = true
+  // add dependencies to classpath
+  copyConfigurable = sourceSets.main.get().runtimeClasspath.filter { it.name.endsWith(".jar") }
+  jvmOptions = jvmOpts.toMutableSet()
+}
+
+val createExe by tasks.createExe
+distributions {
+  main {
+    contents {
+      from(createExe.outputs) {
+        include("*.exe")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+      }
+    }
+  }
+}
+
+tasks.named<Wrapper>("wrapper") {
+  gradleVersion = "6.6.1"
+}
